6288db00271e298a02541ed0c782e762599d102b
jackmott <jack.mott@gmail.com>
2018-05-27 19:36:33 -0500

    3d

--- src/sse2.rs
+++ src/sse2.rs
@@ -16,6 +16,12 @@ const F2: __m128 = unsafe {
         array: [0.36602540378; 4],
     }.simd
 };
+const F3: __m128 = unsafe {
+    M128Array {
+        array:[1.0/3.0;4]
+    }.simd
+};
+
 const G2: __m128 = unsafe {
     M128Array {
         array: [0.2113248654; 4],
@@ -24,6 +30,11 @@ const G2: __m128 = unsafe {
 const G22: __m128 = unsafe {
     M128Array {
         array: [2.0 * 0.2113248654; 4],
+    }.simd
+};
+const G3: __m128 = unsafe {
+    M128Array {
+        array: [1.0/6.0;4]
     }.simd
 };
 const POINT_FIVE: __m128 = unsafe { M128Array { array: [0.5; 4] }.simd };
@@ -253,6 +264,81 @@ unsafe fn turbulence_2d_sse2 (x : __m128 ,y: __m128 , freq : __m128 , lac: f32, 
 	}
 
 	result
+}
+unsafe fn simplex_3d_sse2(x:__m128, y:__m128,z:__m128) -> __m128 {
+
+    let s = M128Array{ simd:_mm_mul_ps(F3,_mm_add_ps(x,_mm_add_ps(y,z)))};
+    let ips = M128Array{ simd: _mm_add_ps(x,s.simd)};
+    let jps = M128Array{ simd: _mm_add_ps(y,s.simd)};
+    let kps = M128Array{ simd: _mm_add_ps(z,s.simd)};
+
+    let i = M128iArray{ array: [
+        ips.array[0].floor() as i32,
+        ips.array[1].floor() as i32,
+        ips.array[2].floor() as i32,
+        ips.array[3].floor() as i32]};
+    let j = M128iArray{ array: [
+        jps.array[0].floor() as i32,
+        jps.array[1].floor() as i32,
+        jps.array[2].floor() as i32,
+        jps.array[3].floor() as i32]};
+    let k = M128iArray{ array: [
+        kps.array[0].floor() as i32,
+        kps.array[1].floor() as i32,
+        kps.array[2].floor() as i32,
+        kps.array[3].floor() as i32]};
+    
+    let t = _mm_mul_ps(_mm_cvtepi32_ps(_mm_add_epi32(i.simd, _mm_add_epi32(j.simd, k.simd))), G3);
+	let X0 = _mm_sub_ps(_mm_cvtepi32_ps(i.simd), t);
+	let Y0 = _mm_sub_ps(_mm_cvtepi32_ps(j.simd), t);
+	let Z0 = _mm_sub_ps(_mm_cvtepi32_ps(k.simd), t);
+	let x0 = _mm_sub_ps(x, X0);
+	let y0 = _mm_sub_ps(y, Y0);
+	let z0 = _mm_sub_ps(z, Z0);
+
+	let i1 = M128iArray{ 
+        simd: _mm_and_si128(_mm_set1_epi32(1), _mm_and_si128(_mm_castps_si128(_mm_cmpge_ps(x0, y0)), _mm_castps_si128(_mm_cmpge_ps(x0, z0))))
+    };
+	let j1 = M128iArray{
+        simd: _mm_and_si128(_mm_set1_epi32(1), _mm_and_si128(_mm_castps_si128(_mm_cmpgt_ps(y0, x0)), _mm_castps_si128(_mm_cmpgt_ps(y0, z0))))
+    };
+	let k1 = M128iArray{
+        simd: _mm_and_si128(_mm_set1_epi32(1), _mm_and_si128(_mm_castps_si128(_mm_cmpgt_ps(z0, x0)), _mm_castps_si128(_mm_cmpgt_ps(z0, y0))))
+    };
+
+	//for i2
+	let yx_xz = _mm_and_si128(_mm_castps_si128(_mm_cmpge_ps(x0, y0)), _mm_castps_si128(_mm_cmplt_ps(x0, z0)));
+	let zx_xy = _mm_and_si128(_mm_castps_si128(_mm_cmpge_ps(x0, z0)), _mm_castps_si128(_mm_cmplt_ps(x0, y0)));
+
+	//for j2
+	let xy_yz = _mm_and_si128(_mm_castps_si128(_mm_cmplt_ps(x0, y0)), _mm_castps_si128(_mm_cmplt_ps(y0, z0)));
+	let zy_yx = _mm_and_si128(_mm_castps_si128(_mm_cmpge_ps(y0, z0)), _mm_castps_si128(_mm_cmpge_ps(x0, y0)));
+
+	//for k2
+	let yz_zx = _mm_and_si128(_mm_castps_si128(_mm_cmplt_ps(y0, z0)), _mm_castps_si128(_mm_cmpge_ps(x0, z0)));
+	let xz_zy = _mm_and_si128(_mm_castps_si128(_mm_cmplt_ps(x0, z0)), _mm_castps_si128(_mm_cmpge_ps(y0, z0)));
+
+    let i2 = M128iArray{ 
+        simd: _mm_and_si128(_mm_set1_epi32(1), _mm_or_si128(i1.simd, _mm_or_si128(yx_xz, zx_xy)))
+    };
+	let j2 = M128iArray{
+        simd: _mm_and_si128(_mm_set1_epi32(1), _mm_or_si128(j1.simd, _mm_or_si128(xy_yz, zy_yx)))
+    };
+	let k2 = M128iArray {
+        simd: _mm_and_si128(_mm_set1_epi32(1), _mm_or_si128(k1.simd, _mm_or_si128(yz_zx, xz_zy)))
+    };
+
+    let x1 = _mm_add_ps(_mm_sub_ps(x0, _mm_cvtepi32_ps(i1.simd)), G3);
+	let y1 = _mm_add_ps(_mm_sub_ps(y0, _mm_cvtepi32_ps(j1.simd)), G3);
+	let z1 = _mm_add_ps(_mm_sub_ps(z0, _mm_cvtepi32_ps(k1.simd)), G3);
+	let x2 = _mm_add_ps(_mm_sub_ps(x0, _mm_cvtepi32_ps(i2.simd)), F3);
+	let y2 = _mm_add_ps(_mm_sub_ps(y0, _mm_cvtepi32_ps(j2.simd)), F3);
+	let z2 = _mm_add_ps(_mm_sub_ps(z0, _mm_cvtepi32_ps(k2.simd)), F3);
+	let x3 = _mm_add_ps(_mm_sub_ps(x0, _mm_set1_ps(1.0)), POINT_FIVE);
+	let y3 = _mm_add_ps(_mm_sub_ps(y0, _mm_set1_ps(1.0)), POINT_FIVE);
+	let z3 = _mm_add_ps(_mm_sub_ps(z0, _mm_set1_ps(1.0)), POINT_FIVE);
+
+    ips.simd
 }
 #[cfg(any(target_arch = "x86_64"))]
 pub fn helper(a:f32,b:f32,c:f32,d:f32) -> (f32, f32, f32, f32) {
