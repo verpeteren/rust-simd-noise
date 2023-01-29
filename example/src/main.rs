f223aea2c00f9bf38752ce9de7635b51a404ae7c
peter <peter.reijnders@verpeteren.nl>
2023-01-29 13:50:55 +0100

    patch: example; width and height command line arguments

--- example/src/main.rs
+++ example/src/main.rs
@@ -1,11 +1,23 @@
+use clap::Parser;
 use minifb::{Key, Window, WindowOptions};
 
 const WIDTH: usize = 1920;
 const HEIGHT: usize = 1080;
 
+#[derive(Parser, Debug)]
+#[command(author, version, about, long_about = None)]
+struct Args {
+    #[clap(short, long, value_parser, default_value_t = WIDTH, help="The width of the generated image")]
+    pub width: usize,
+
+    #[clap(long, value_parser, default_value_t = HEIGHT, help="The height of the generated image")]
+    pub height: usize,
+}
+
 fn main() {
-    let width = WIDTH;
-    let height = HEIGHT;
+    let args = Args::parse();
+    let width = args.width;
+    let height = args.height;
 
     let noise = simdnoise::NoiseBuilder::ridge_3d_offset(1200.0, width, 200.0, height, 1.0, 1)
         .with_freq(1.2)
