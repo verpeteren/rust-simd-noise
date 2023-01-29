29e6c8ce32d9aafa12ef56b939b5a4e63bb42c29
peter <peter.reijnders@verpeteren.nl>
2023-01-29 13:48:26 +0100

    patch: example; use variables and set them once for the WIDTH and HEIGHT constants

--- example/src/main.rs
+++ example/src/main.rs
@@ -4,15 +4,18 @@ const WIDTH: usize = 1920;
 const HEIGHT: usize = 1080;
 
 fn main() {
-    let noise = simdnoise::NoiseBuilder::ridge_3d_offset(1200.0, WIDTH, 200.0, HEIGHT, 1.0, 1)
+    let width = WIDTH;
+    let height = HEIGHT;
+
+    let noise = simdnoise::NoiseBuilder::ridge_3d_offset(1200.0, width, 200.0, height, 1.0, 1)
         .with_freq(1.2)
         .with_octaves(8)
         .generate_scaled(0.0, 255.0);
     let buffer: Vec<u32> = noise.iter().map(|x| *x as u32).collect();
     let mut window = Window::new(
         "Test - ESC to exit",
-        WIDTH,
-        HEIGHT,
+        width,
+        height,
         WindowOptions::default(),
     )
     .unwrap_or_else(|e| {
@@ -22,6 +25,6 @@ fn main() {
     window.limit_update_rate(Some(std::time::Duration::from_micros(16600)));
 
     while window.is_open() && !window.is_key_down(Key::Escape) {
-        window.update_with_buffer(&buffer, WIDTH, HEIGHT).unwrap();
+        window.update_with_buffer(&buffer, width, height).unwrap();
     }
 }
