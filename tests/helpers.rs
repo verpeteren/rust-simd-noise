01401ca4fc2dcd099a3992f3b5f6a1ff99a1adda
peter <peter.reijnders@verpeteren.nl>
2023-02-20 07:24:42 +0100

    chore: create test helpers module

---
+++ tests/helpers.rs
@@ -0,0 +1,61 @@
+use std::fs::metadata;
+use std::fs::File;
+use std::io::prelude::*;
+use std::io::BufReader;
+
+use byteorder::{ByteOrder, LittleEndian, WriteBytesExt};
+pub fn read_from_file_f32(file_name: &str) -> std::io::Result<Vec<f32>> {
+    let file = File::open(file_name)?;
+    let len = metadata(file_name)?.len();
+    let mut noise = Vec::with_capacity((len / 4) as usize);
+    let mut reader = BufReader::new(file);
+    let mut block: [u8; 4] = [0; 4];
+    loop {
+        match reader.read_exact(&mut block) {
+            Ok(_) => {
+                let f = LittleEndian::read_f32(&mut block);
+                noise.push(f);
+            }
+            Err(_) => {
+                break;
+            }
+        };
+    }
+    Ok(noise)
+}
+
+pub fn read_from_file_f64(file_name: &str) -> std::io::Result<Vec<f64>> {
+    let file = File::open(file_name)?;
+    let len = metadata(file_name)?.len();
+    let mut noise = Vec::with_capacity((len / 4) as usize);
+    let mut reader = BufReader::new(file);
+    let mut block: [u8; 4] = [0; 4];
+    loop {
+        match reader.read_exact(&mut block) {
+            Ok(_) => {
+                let f = LittleEndian::read_f64(&mut block);
+                noise.push(f);
+            }
+            Err(_) => {
+                break;
+            }
+        };
+    }
+    Ok(noise)
+}
+
+pub fn save_to_file_f32(file_name: &str, noise: &[f32]) -> std::io::Result<()> {
+    let mut file = File::create(file_name)?;
+    for f in noise {
+        file.write_f32::<LittleEndian>(*f)?;
+    }
+    Ok(())
+}
+
+pub fn save_to_file_f64(file_name: &str, noise: &[f64]) -> std::io::Result<()> {
+    let mut file = File::create(file_name)?;
+    for f in noise {
+        file.write_f64::<LittleEndian>(*f)?;
+    }
+    Ok(())
+}
--- tests/noisebuilder.rs
+++ tests/noisebuilder.rs
@@ -1,67 +1,7 @@
 use simdnoise::NoiseBuilder;
 
-use std::fs::metadata;
-use std::fs::File;
-use std::io::prelude::*;
-use std::io::BufReader;
-
-use byteorder::{ByteOrder, LittleEndian, WriteBytesExt};
-
-fn read_from_file_f32(file_name: &str) -> std::io::Result<Vec<f32>> {
-    let file = File::open(file_name)?;
-    let len = metadata(file_name)?.len();
-    let mut noise = Vec::with_capacity((len / 4) as usize);
-    let mut reader = BufReader::new(file);
-    let mut block: [u8; 4] = [0; 4];
-    loop {
-        match reader.read_exact(&mut block) {
-            Ok(_) => {
-                let f = LittleEndian::read_f32(&mut block);
-                noise.push(f);
-            }
-            Err(_) => {
-                break;
-            }
-        };
-    }
-    Ok(noise)
-}
-
-fn read_from_file_f64(file_name: &str) -> std::io::Result<Vec<f64>> {
-    let file = File::open(file_name)?;
-    let len = metadata(file_name)?.len();
-    let mut noise = Vec::with_capacity((len / 4) as usize);
-    let mut reader = BufReader::new(file);
-    let mut block: [u8; 4] = [0; 4];
-    loop {
-        match reader.read_exact(&mut block) {
-            Ok(_) => {
-                let f = LittleEndian::read_f64(&mut block);
-                noise.push(f);
-            }
-            Err(_) => {
-                break;
-            }
-        };
-    }
-    Ok(noise)
-}
-
-fn save_to_file_f32(file_name: &str, noise: &[f32]) -> std::io::Result<()> {
-    let mut file = File::create(file_name)?;
-    for f in noise {
-        file.write_f32::<LittleEndian>(*f)?;
-    }
-    Ok(())
-}
-
-fn save_to_file_f64(file_name: &str, noise: &[f64]) -> std::io::Result<()> {
-    let mut file = File::create(file_name)?;
-    for f in noise {
-        file.write_f64::<LittleEndian>(*f)?;
-    }
-    Ok(())
-}
+mod helpers;
+use helpers::{read_from_file_f32, save_to_file_f32};
 
 mod noise {
     use super::*;
