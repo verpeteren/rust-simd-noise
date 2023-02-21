3b0b29ce6a83bc83d33fbf3e1e6c702be9123693
peter <peter.reijnders@verpeteren.nl>
2023-02-21 07:42:53 +0100

    patch: extract TurbulenceSettings to its own module

--- src/settings/turbulence_settings.rs
+++ src/settings/turbulence_settings.rs
@@ -7,49 +7,44 @@ pub use crate::noise_builder::NoiseBuilder;
 pub use crate::noise_dimensions::NoiseDimensions;
 pub use crate::noise_type::NoiseType;
 
-mod cellular_settings;
-pub use cellular_settings::CellularSettings;
-mod cellular2_settings;
-pub use cellular2_settings::Cellular2Settings;
-mod fbm_settings;
-pub use fbm_settings::FbmSettings;
-mod ridge_settings;
-pub use ridge_settings::RidgeSettings;
-mod turbulence_settings;
-pub use turbulence_settings::TurbulenceSettings;
-
 #[derive(Copy, Clone)]
-pub struct GradientSettings {
+pub struct TurbulenceSettings {
     dim: NoiseDimensions,
     pub freq_x: f32,
     pub freq_y: f32,
     pub freq_z: f32,
     pub freq_w: f32,
+    pub lacunarity: f32,
+    pub gain: f32,
+    pub octaves: u8,
 }
 
-impl DimensionalBeing for GradientSettings {
+impl DimensionalBeing for TurbulenceSettings {
     fn get_dimensions(&self) -> NoiseDimensions {
         return self.dim;
     }
 }
 
-impl GradientSettings {
-    pub fn default(dim: NoiseDimensions) -> GradientSettings {
-        GradientSettings {
+impl TurbulenceSettings {
+    pub fn default(dim: NoiseDimensions) -> TurbulenceSettings {
+        TurbulenceSettings {
             dim,
             freq_x: 0.02,
             freq_y: 0.02,
             freq_z: 0.02,
             freq_w: 0.02,
+            lacunarity: 0.5,
+            gain: 2.0,
+            octaves: 3,
         }
     }
 
-    pub fn with_seed(&mut self, seed: i32) -> &mut GradientSettings {
+    pub fn with_seed(&mut self, seed: i32) -> &mut TurbulenceSettings {
         self.dim.seed = seed;
         self
     }
 
-    pub fn with_freq(&mut self, freq: f32) -> &mut GradientSettings {
+    pub fn with_freq(&mut self, freq: f32) -> &mut TurbulenceSettings {
         self.freq_x = freq;
         self.freq_y = freq;
         self.freq_z = freq;
@@ -57,13 +52,18 @@ impl GradientSettings {
         self
     }
 
-    pub fn with_freq_2d(&mut self, freq_x: f32, freq_y: f32) -> &mut GradientSettings {
+    pub fn with_freq_2d(&mut self, freq_x: f32, freq_y: f32) -> &mut TurbulenceSettings {
         self.freq_x = freq_x;
         self.freq_y = freq_y;
         self
     }
 
-    pub fn with_freq_3d(&mut self, freq_x: f32, freq_y: f32, freq_z: f32) -> &mut GradientSettings {
+    pub fn with_freq_3d(
+        &mut self,
+        freq_x: f32,
+        freq_y: f32,
+        freq_z: f32,
+    ) -> &mut TurbulenceSettings {
         self.freq_x = freq_x;
         self.freq_y = freq_y;
         self.freq_z = freq_z;
@@ -76,7 +76,7 @@ impl GradientSettings {
         freq_y: f32,
         freq_z: f32,
         freq_w: f32,
-    ) -> &mut GradientSettings {
+    ) -> &mut TurbulenceSettings {
         self.freq_x = freq_x;
         self.freq_y = freq_y;
         self.freq_z = freq_z;
@@ -84,10 +84,25 @@ impl GradientSettings {
         self
     }
 
+    pub fn with_lacunarity(&mut self, lacunarity: f32) -> &mut TurbulenceSettings {
+        self.lacunarity = lacunarity;
+        self
+    }
+
+    pub fn with_gain(&mut self, gain: f32) -> &mut TurbulenceSettings {
+        self.gain = gain;
+        self
+    }
+
+    pub fn with_octaves(&mut self, octaves: u8) -> &mut TurbulenceSettings {
+        self.octaves = octaves;
+        self
+    }
+
     /// If you want to call noise functions by hand, call wrap on the settings
     /// to get back a NoiseType to call the noise functions with
     pub fn wrap(self) -> NoiseType {
-        NoiseType::Gradient(self)
+        NoiseType::Turbulence(self)
     }
 
     /// Generate a chunk of noise based on your settings, and the min and max value
@@ -95,10 +110,10 @@ impl GradientSettings {
     pub fn generate(self) -> (Vec<f32>, f32, f32) {
         let d = self.dim.dim;
         match d {
-            1 => get_1d_noise!(&NoiseType::Gradient(self)),
-            2 => get_2d_noise!(&NoiseType::Gradient(self)),
-            3 => get_3d_noise!(&NoiseType::Gradient(self)),
-            4 => get_4d_noise!(&NoiseType::Gradient(self)),
+            1 => get_1d_noise!(&NoiseType::Turbulence(self)),
+            2 => get_2d_noise!(&NoiseType::Turbulence(self)),
+            3 => get_3d_noise!(&NoiseType::Turbulence(self)),
+            4 => get_4d_noise!(&NoiseType::Turbulence(self)),
             _ => panic!("not implemented"),
         }
     }
@@ -110,10 +125,10 @@ impl GradientSettings {
         new_self.dim.min = min;
         new_self.dim.max = max;
         match d {
-            1 => get_1d_scaled_noise!(&NoiseType::Gradient(new_self)),
-            2 => get_2d_scaled_noise!(&NoiseType::Gradient(new_self)),
-            3 => get_3d_scaled_noise!(&NoiseType::Gradient(new_self)),
-            4 => get_4d_scaled_noise!(&NoiseType::Gradient(new_self)),
+            1 => get_1d_scaled_noise!(&NoiseType::Turbulence(new_self)),
+            2 => get_2d_scaled_noise!(&NoiseType::Turbulence(new_self)),
+            3 => get_3d_scaled_noise!(&NoiseType::Turbulence(new_self)),
+            4 => get_4d_scaled_noise!(&NoiseType::Turbulence(new_self)),
             _ => panic!("not implemented"),
         }
     }
