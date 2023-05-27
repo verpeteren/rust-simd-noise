836a32ace2e20f063da3e1e80cca98cb7d56a473
peter <peter.reijnders@verpeteren.nl>
2023-05-27 13:03:56 +0200

    minor: disable hash3d for 64 bit

--- src/noise/hash3d_64.rs
+++ src/noise/hash3d_64.rs
@@ -29,6 +29,10 @@ where
 
 #[inline(always)]
 pub unsafe fn hash3d<S: Simd>(seed: i64, i: S::Vi64, j: S::Vi64, k: S::Vi64) -> Hash3d<S> {
+    // This 64 bit variant is not implemented.
+    // The codeblock below is just the 64 bit SIMD instructions with the 32 bit magic numbers.
+    // I don't know what values the Hash3d fields should hold or what magic number are needed for the bit shifts.
+    unimplemented!();
     let mut hash = S::xor_epi64(i, S::set1_epi64(seed));
     hash = S::xor_epi64(j, hash);
     hash = S::xor_epi64(k, hash);
@@ -41,7 +45,6 @@ pub unsafe fn hash3d<S: Simd>(seed: i64, i: S::Vi64, j: S::Vi64, k: S::Vi64) -> 
     Hash3d::new(
         S::castepi64_pd(S::cmplt_epi64(hasha13, S::set1_epi64(8))),
         S::castepi64_pd(S::cmplt_epi64(hasha13, S::set1_epi64(2))),
-        //? magic numbers
         S::castepi64_pd(S::cmpeq_epi64(S::set1_epi64(12), hasha13)),
         S::castepi64_pd(S::slli_epi64(hash, 31)),
         S::castepi64_pd(S::slli_epi64(S::and_epi64(hash, S::set1_epi64(2)), 30)),
