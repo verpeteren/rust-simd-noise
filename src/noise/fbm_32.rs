3c9634b704752cbff6349bd4ed5ab6285ca3ca73
Awowogei <None>
2023-10-02 17:03:53 +0200

    Scale fbm

--- src/noise/fbm_32.rs
+++ src/noise/fbm_32.rs
@@ -11,14 +11,17 @@ pub unsafe fn fbm_1d<S: Simd>(
     seed: i32,
 ) -> S::Vf32 {
     let mut amp = S::set1_ps(1.0);
+    let mut scale = amp;
     let mut result = simplex_1d::<S>(x, seed);
 
     for _ in 1..octaves {
         x = S::mul_ps(x, lacunarity);
         amp = S::mul_ps(amp, gain);
+        scale = S::add_ps(scale, amp);
         result = S::add_ps(result, simplex_1d::<S>(x, seed));
     }
 
+    result = S::div_ps(result, scale);
     result
 }
 
@@ -31,16 +34,19 @@ pub unsafe fn fbm_2d<S: Simd>(
     octaves: u8,
     seed: i32,
 ) -> S::Vf32 {
-    let mut result = simplex_2d::<S>(x, y, seed);
     let mut amp = S::set1_ps(1.0);
+    let mut scale = amp;
+    let mut result = simplex_2d::<S>(x, y, seed);
 
     for _ in 1..octaves {
         x = S::mul_ps(x, lac);
         y = S::mul_ps(y, lac);
         amp = S::mul_ps(amp, gain);
+        scale = S::add_ps(scale, amp);
         result = S::add_ps(S::mul_ps(simplex_2d::<S>(x, y, seed), amp), result);
     }
 
+    result = S::div_ps(result, scale);
     result
 }
 
@@ -54,17 +60,20 @@ pub unsafe fn fbm_3d<S: Simd>(
     octaves: u8,
     seed: i32,
 ) -> S::Vf32 {
-    let mut result = simplex_3d::<S>(x, y, z, seed);
     let mut amp = S::set1_ps(1.0);
+    let mut scale = amp;
+    let mut result = simplex_3d::<S>(x, y, z, seed);
 
     for _ in 1..octaves {
         x = S::mul_ps(x, lac);
         y = S::mul_ps(y, lac);
         z = S::mul_ps(z, lac);
         amp = S::mul_ps(amp, gain);
+        scale = S::add_ps(scale, amp);
         result = S::add_ps(S::mul_ps(simplex_3d::<S>(x, y, z, seed), amp), result);
     }
 
+    result = S::div_ps(result, scale);
     result
 }
 
@@ -79,8 +88,9 @@ pub unsafe fn fbm_4d<S: Simd>(
     octaves: u8,
     seed: i32,
 ) -> S::Vf32 {
-    let mut result = simplex_4d::<S>(x, y, z, w, seed);
     let mut amp = S::set1_ps(1.0);
+    let mut scale = amp;
+    let mut result = simplex_4d::<S>(x, y, z, w, seed);
 
     for _ in 1..octaves {
         x = S::mul_ps(x, lac);
@@ -88,8 +98,10 @@ pub unsafe fn fbm_4d<S: Simd>(
         z = S::mul_ps(z, lac);
         w = S::mul_ps(w, lac);
         amp = S::mul_ps(amp, gain);
+        scale = S::add_ps(scale, amp);
         result = S::add_ps(result, S::mul_ps(simplex_4d::<S>(x, y, z, w, seed), amp));
     }
 
+    result = S::div_ps(result, scale);
     result
 }
--- src/noise/fbm_64.rs
+++ src/noise/fbm_64.rs
@@ -11,14 +11,17 @@ pub unsafe fn fbm_1d<S: Simd>(
     seed: i64,
 ) -> S::Vf64 {
     let mut amp = S::set1_pd(1.0);
+    let mut scale = amp;
     let mut result = simplex_1d::<S>(x, seed);
 
     for _ in 1..octaves {
         x = S::mul_pd(x, lacunarity);
         amp = S::mul_pd(amp, gain);
+        scale = S::add_pd(scale, amp);
         result = S::add_pd(result, simplex_1d::<S>(x, seed));
     }
 
+    result = S::div_pd(result, scale);
     result
 }
 
@@ -31,16 +34,19 @@ pub unsafe fn fbm_2d<S: Simd>(
     octaves: u8,
     seed: i64,
 ) -> S::Vf64 {
-    let mut result = simplex_2d::<S>(x, y, seed);
     let mut amp = S::set1_pd(1.0);
+    let mut scale = amp;
+    let mut result = simplex_2d::<S>(x, y, seed);
 
     for _ in 1..octaves {
         x = S::mul_pd(x, lac);
         y = S::mul_pd(y, lac);
         amp = S::mul_pd(amp, gain);
+        scale = S::add_pd(scale, amp);
         result = S::add_pd(S::mul_pd(simplex_2d::<S>(x, y, seed), amp), result);
     }
 
+    result = S::div_pd(result, scale);
     result
 }
 
@@ -54,15 +60,20 @@ pub unsafe fn fbm_3d<S: Simd>(
     octaves: u8,
     seed: i64,
 ) -> S::Vf64 {
-    let mut result = simplex_3d::<S>(x, y, z, seed);
     let mut amp = S::set1_pd(1.0);
+    let mut scale = amp;
+    let mut result = simplex_3d::<S>(x, y, z, seed);
+
     for _ in 1..octaves {
         x = S::mul_pd(x, lac);
         y = S::mul_pd(y, lac);
         z = S::mul_pd(z, lac);
         amp = S::mul_pd(amp, gain);
+        scale = S::add_pd(scale, amp);
         result = S::add_pd(S::mul_pd(simplex_3d::<S>(x, y, z, seed), amp), result);
     }
+
+    result = S::div_pd(result, scale);
     result
 }
 
@@ -77,8 +88,9 @@ pub unsafe fn fbm_4d<S: Simd>(
     octaves: u8,
     seed: i64,
 ) -> S::Vf64 {
-    let mut result = simplex_4d::<S>(x, y, z, w, seed);
     let mut amp = S::set1_pd(1.0);
+    let mut scale = amp;
+    let mut result = simplex_4d::<S>(x, y, z, w, seed);
 
     for _ in 1..octaves {
         x = S::mul_pd(x, lac);
@@ -86,8 +98,10 @@ pub unsafe fn fbm_4d<S: Simd>(
         z = S::mul_pd(z, lac);
         w = S::mul_pd(w, lac);
         amp = S::mul_pd(amp, gain);
+        scale = S::add_pd(scale, amp);
         result = S::add_pd(result, S::mul_pd(simplex_4d::<S>(x, y, z, w, seed), amp));
     }
 
+    result = S::div_pd(result, scale);
     result
 }
