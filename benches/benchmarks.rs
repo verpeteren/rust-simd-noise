428ae7a6a071f47d9629ab6aab5ed896a51e5b74
Peter Reijnders <peter.reijnders@verpeteren.nl>
2024-11-20 09:12:19 +0100

    patch: port benchmarks to simdeez v2

--- benches/benchmarks.rs
+++ benches/benchmarks.rs
@@ -1,26 +1,29 @@
 extern crate criterion;
 extern crate simdeez;
 extern crate simdnoise;
-use criterion::*;
-use simdnoise::*;
+
 use std::time::Duration;
 
+use criterion::*;
+use simdeez::prelude::*;
+
 use simdnoise::intrinsics::*;
+use simdnoise::*;
 
 fn d4(c: &mut Criterion) {
     let setting = NoiseBuilder::fbm_4d(8, 8, 8, 8).wrap();
     let mut group = c.benchmark_group("fbm4d");
     group.bench_function("scalar 4d", move |b| {
-        b.iter(|| unsafe { scalar::get_4d_noise(&setting) })
+        b.iter(|| unsafe { scalar::get_4d_noise::<simdeez::scalar::Scalar>(&setting) })
     });
     group.bench_function("sse2 4d", move |b| {
-        b.iter(|| unsafe { sse2::get_4d_noise(&setting) })
+        b.iter(|| unsafe { sse2::get_4d_noise::<simdeez::Sse2>(&setting) })
     });
     group.bench_function("sse41 4d", move |b| {
-        b.iter(|| unsafe { sse41::get_4d_noise(&setting) })
+        b.iter(|| unsafe { sse41::get_4d_noise::<simdeez::Sse41>(&setting) })
     });
     group.bench_function("avx2 4d", move |b| {
-        b.iter(|| unsafe { avx2::get_4d_noise(&setting) })
+        b.iter(|| unsafe { avx2::get_4d_noise::<simdeez::Avx2>(&setting) })
     });
     group
         .sample_size(10)
@@ -32,16 +35,16 @@ fn d3(c: &mut Criterion) {
     let setting = NoiseBuilder::fbm_3d(64, 64, 64).wrap();
     let mut group = c.benchmark_group("fbm3d");
     group.bench_function("scalar 3d", move |b| {
-        b.iter(|| unsafe { scalar::get_3d_noise(&setting) })
+        b.iter(|| unsafe { scalar::get_3d_noise::<simdeez::scalar::Scalar>(&setting) })
     });
     group.bench_function("sse2 3d", move |b| {
-        b.iter(|| unsafe { sse2::get_3d_noise(&setting) })
+        b.iter(|| unsafe { sse2::get_3d_noise::<simdeez::Sse2>(&setting) })
     });
     group.bench_function("sse41 3d", move |b| {
-        b.iter(|| unsafe { sse41::get_3d_noise(&setting) })
+        b.iter(|| unsafe { sse41::get_3d_noise::<simdeez::Sse41>(&setting) })
     });
     group.bench_function("avx2 3d", move |b| {
-        b.iter(|| unsafe { avx2::get_3d_noise(&setting) })
+        b.iter(|| unsafe { avx2::get_3d_noise::<simdeez::Avx2>(&setting) })
     });
     group
         .sample_size(10)
@@ -53,16 +56,16 @@ fn d2(c: &mut Criterion) {
     let setting = NoiseBuilder::fbm_2d(3840, 2160).wrap();
     let mut group = c.benchmark_group("fbm2d");
     group.bench_function("scalar 2d", move |b| {
-        b.iter(|| unsafe { scalar::get_2d_noise(&setting) })
+        b.iter(|| unsafe { scalar::get_2d_noise::<simdeez::scalar::Scalar>(&setting) })
     });
     group.bench_function("sse2 2d", move |b| {
-        b.iter(|| unsafe { sse2::get_2d_noise(&setting) })
+        b.iter(|| unsafe { sse2::get_2d_noise::<simdeez::Sse2>(&setting) })
     });
     group.bench_function("sse41 2d", move |b| {
-        b.iter(|| unsafe { sse41::get_2d_noise(&setting) })
+        b.iter(|| unsafe { sse41::get_2d_noise::<simdeez::Sse41>(&setting) })
     });
     group.bench_function("avx2 2d", move |b| {
-        b.iter(|| unsafe { avx2::get_2d_noise(&setting) })
+        b.iter(|| unsafe { avx2::get_2d_noise::<simdeez::Avx2>(&setting) })
     });
     group
         .sample_size(10)
@@ -74,16 +77,16 @@ fn d1(c: &mut Criterion) {
     let setting = NoiseBuilder::fbm_1d(1024).wrap();
     let mut group = c.benchmark_group("fbm1d");
     group.bench_function("scalar 1d", move |b| {
-        b.iter(|| unsafe { scalar::get_1d_noise(&setting) })
+        b.iter(|| unsafe { scalar::get_1d_noise::<simdeez::scalar::Scalar>(&setting) })
     });
     group.bench_function("sse2 1d", move |b| {
-        b.iter(|| unsafe { sse2::get_1d_noise(&setting) })
+        b.iter(|| unsafe { sse2::get_1d_noise::<simdeez::Sse2>(&setting) })
     });
     group.bench_function("sse41 1d", move |b| {
-        b.iter(|| unsafe { sse41::get_1d_noise(&setting) })
+        b.iter(|| unsafe { sse41::get_1d_noise::<simdeez::Sse41>(&setting) })
     });
     group.bench_function("avx2 1d", move |b| {
-        b.iter(|| unsafe { avx2::get_1d_noise(&setting) })
+        b.iter(|| unsafe { avx2::get_1d_noise::<simdeez::Avx2>(&setting) })
     });
     group
         .sample_size(10)
@@ -97,16 +100,16 @@ fn d2_cell(c: &mut Criterion) {
         .wrap();
     let mut group = c.benchmark_group("cellular2d");
     group.bench_function("scalar 2d", move |b| {
-        b.iter(|| unsafe { scalar::get_2d_noise(&setting) })
+        b.iter(|| unsafe { scalar::get_2d_noise::<simdeez::scalar::Scalar>(&setting) })
     });
     group.bench_function("sse2 2d", move |b| {
-        b.iter(|| unsafe { sse2::get_2d_noise(&setting) })
+        b.iter(|| unsafe { sse2::get_2d_noise::<simdeez::Sse2>(&setting) })
     });
     group.bench_function("sse41 2d", move |b| {
-        b.iter(|| unsafe { sse41::get_2d_noise(&setting) })
+        b.iter(|| unsafe { sse41::get_2d_noise::<simdeez::Sse41>(&setting) })
     });
     group.bench_function("avx2 2d", move |b| {
-        b.iter(|| unsafe { avx2::get_2d_noise(&setting) })
+        b.iter(|| unsafe { avx2::get_2d_noise::<simdeez::Avx2>(&setting) })
     });
     group
         .sample_size(10)
@@ -118,16 +121,16 @@ fn d3_cell(c: &mut Criterion) {
     let setting = NoiseBuilder::cellular_3d(128, 128, 128).wrap();
     let mut group = c.benchmark_group("cellular3d");
     group.bench_function("scalar 3d", move |b| {
-        b.iter(|| unsafe { scalar::get_3d_noise(&setting) })
+        b.iter(|| unsafe { scalar::get_3d_noise::<simdeez::scalar::Scalar>(&setting) })
     });
     group.bench_function("sse2 3d", move |b| {
-        b.iter(|| unsafe { sse2::get_3d_noise(&setting) })
+        b.iter(|| unsafe { sse2::get_3d_noise::<simdeez::Sse2>(&setting) })
     });
     group.bench_function("sse41 3d", move |b| {
-        b.iter(|| unsafe { sse41::get_3d_noise(&setting) })
+        b.iter(|| unsafe { sse41::get_3d_noise::<simdeez::Sse41>(&setting) })
     });
     group.bench_function("avx2 3d", move |b| {
-        b.iter(|| unsafe { avx2::get_3d_noise(&setting) })
+        b.iter(|| unsafe { avx2::get_3d_noise::<simdeez::Avx2>(&setting) })
     });
     group
         .sample_size(10)
